77726d542f2e231725c942c99544f69f613736c6
StelianRBG <steliantodorichkov03@gmail.com>
2022-02-16 16:16:06 +0200

    Fix Error Handling

--- MarkMe/MarkMe/View Controllers/LoginVC.swift
+++ MarkMe/MarkMe/View Controllers/LoginVC.swift
@@ -21,7 +21,7 @@ class LoginVC: UIViewController {
 }
 
 extension LoginVC {
-    @IBAction func signIN(_ sender: UIButton) {
+    @IBAction func signIn(_ sender: UIButton) {
         // get data from fields
         guard let username = usernameField.text else { return }
         guard let password = passwordField.text else { return }
@@ -35,11 +35,7 @@ extension LoginVC {
         let db = Firestore.firestore()
         // find user
         db.collection("User").whereField("username", isEqualTo: username).getDocuments() { [weak self] (querySnapshot, err) in
-            guard err == nil else {
-                print("Error in searching in db")
-                return
-            }
-            guard querySnapshot?.documents.isEmpty == false else {
+            guard err == nil, querySnapshot?.documents.isEmpty == false else {
                 self?.showAlert(alertMessage: "User is not found!", title: "Error")
                 return
             }
