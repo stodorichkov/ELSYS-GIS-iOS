4d98effa7819f13ca8a482a1eb788ea79d94589d
StelianRBG <steliantodorichkov03@gmail.com>
2022-02-23 19:04:27 +0200

    Remove old comments

--- MarkMe/MarkMe/Screens/Main/ViewController.swift
+++ MarkMe/MarkMe/Screens/Main/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-//import FirebaseAuth
 
 class ViewController: UIViewController {
     
@@ -31,29 +30,3 @@ extension ViewController: MainViewModelDelegate{
     
     
 }
-
-/*extension ViewController {
-    func makeLoad() {
-        var progress: Float = 0.0
-        progressBar.progress = progress
-        Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true, block: { [weak self] (timer) in
-            progress += 0.1
-            self?.progressBar.progress = progress
-            if progress == 1 {
-                timer.invalidate()
-            }
-        })
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { [weak self] (timer) in
-            Auth.auth().addStateDidChangeListener() { [weak self] (auth, user) in
-                if user != nil {
-                    self?.changeScreen(storyboardName: "Tabs", viewControllerId: "tabs", transition: .crossDissolve)
-                }
-                else {
-                    self?.changeScreen(storyboardName: "Authentication", viewControllerId: "login", transition: .crossDissolve)
-                }
-            }
-            
-        })
-    }
-}*/
-
