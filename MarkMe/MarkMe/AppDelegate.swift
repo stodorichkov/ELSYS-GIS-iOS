9b18908becbedaa470985cb114e2ee198d005faa
StelianRBG <steliantodorichkov03@gmail.com>
2022-03-25 10:30:44 +0200

    Remove min version for google

--- MarkMe/MarkMe/AppDelegate.swift
+++ MarkMe/MarkMe/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-    @available(iOS 9.0, *)
+    
     func application(
         _ app: UIApplication,
         open url: URL,
